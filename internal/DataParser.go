77a1ca24f0d498b428a29f7d8936b66e8035ed24
James Leung <james.warren.leung@gmail.com>
2020-01-01 18:45:03 -0800

    implemented get pokemon on API

--- internal/DataParser.go
+++ internal/DataParser.go
@@ -1,83 +1,30 @@
 package internal
 
 import (
-  "encoding/json"
-  "fmt"
-  "io/ioutil"
-  "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 
-  "github.com/jwl/nimbasa-grand-central/pokemon"
+	"github.com/jwl/nimbasa-grand-central/pokemon"
 )
 
-// Extract Pokemon data from stats.json
-
-// GetObjectFromJSON extracts a data object from a json object
-//func GetObjectFromJSON(jsonObject string) string {
-  //testJSON := `{"testkey":"testvalue"}`
-
-  //var parsedJSON string
-
-  //json.Unmarshal([]byte(testJSON), &parsedJSON)
-
-  //return parsedJSON
-
-//}
-
-//func GetPokemonFromFile(path string) []map[string]interface{} {
 func GetPokemonFromFile(path string) []pokemon.Pkmn {
-  //jsonFile, err := os.Open("./pokemondata/stats.json")
-  jsonFile, err := os.Open(path)
+	jsonFile, err := os.Open(path)
 
-  if err != nil {
-    fmt.Println(err)
-    return nil
-  } else {
-    fmt.Println("Successfully opened stats.json")
-  }
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
 
-  defer jsonFile.Close()
+	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  //var pokemonFromFile pokemon.PokemonArray
+	var results []pokemon.Pkmn
 
-  //fmt.Println("byteValue is: " + string(byteValue))
+	// Unmarshal or Decode the JSON to the interface.
+	json.Unmarshal(byteValue, &results)
 
-  // Declared an empty interface of type Array
-  //var results []map[string]interface{}
-  var results []pokemon.Pkmn
-
-  // Unmarshal or Decode the JSON to the interface.
-  json.Unmarshal(byteValue, &results)
-
-  //for key, result := range results {
-    //fmt.Println("Reading Value for Key :", key)
-    //fmt.Println("\tnumber :", result["number"], "/ Name :", result["name"])
-  //}
-
-  //json.Unmarshal(byteValue, &pokemonFromFile)
-
-  //for key, pokemon := range pokemonFromFile {
-    //fmt.Println("reading value for key: ", key)
-    //fmt.Println("\tnumber: ", pokemon["number"], "/ Name: ", pokemon["name"])
-  //}
-
-  //err = json.Unmarshal(byteValue, &pokemonFromFile)
-  //if err != nil {
-  //fmt.Println("error:", err)
-  //}
-
-  //fmt.Println("json unmarshaled, about to print contents of pokemonFromFile")
-  //fmt.Println("pokemonFromFile is: " + pokemonFromFile)
-  //fmt.Println(json.MarshalIndent(pokemonFromFile, "", "    "))
-  //fmt.Println("len(pokemonFromFile.PokemonArray) is: " + strconv.Itoa(len(pokemonFromFile.PokemonArray)))
-  //fmt.Println("pokemonFromFile: " + pokemonFromFile)
-
-  //for i := 0; i < len(pokemonFromFile.PokemonArray); i++ {
-  //fmt.Println("Pokemon Name: " + pokemonFromFile.PokemonArray[i].Name)
-  //fmt.Println("Pokemon Number: " + strconv.Itoa(pokemonFromFile.PokemonArray[i].Number))
-  //}
-
-  //return ""
-  return results
+	return results
 }
